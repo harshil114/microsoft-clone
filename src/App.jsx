892704779d66bc338127634ab7b8877fef1970c1
harshil114 <harshilvispute114@gmail.com>
2024-06-10 13:01:45 +0530

    Add List.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Card from "./components/Card";
+import List from "./components/List";
 import Navbar from "./components/Navbar";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <>
       <Navbar />
       <Card />
+      <List/>
     </>
   );
 }
